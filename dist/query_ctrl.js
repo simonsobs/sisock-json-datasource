54947fd46dc1a4e98de69c76aa4e0f92174b8583
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-05-27 10:24:10 -0400

    Rebuild ./dist

--- dist/query_ctrl.js
+++ dist/query_ctrl.js
@@ -26,15 +26,69 @@ var GenericDatasourceQueryCtrl = exports.GenericDatasourceQueryCtrl = function (
     var _this = _possibleConstructorReturn(this, (GenericDatasourceQueryCtrl.__proto__ || Object.getPrototypeOf(GenericDatasourceQueryCtrl)).call(this, $scope, $injector));
 
     _this.scope = $scope;
-    _this.target.target = _this.target.target || 'select metric';
+
+    // The casc_max should match the number of dropdowns defined in
+    // query.editor.html.  There is not much internal checking on this
+    // limit, though.
+    _this.casc_max = 10;
+
+    _this.casc_level = 0;
+    for (var i = 0; i < _this.casc_max; i++) {
+      _this['casc_data' + i] = '';
+    }_this.target.target = _this.target.target || 'select metric';
     _this.target.type = _this.target.type || 'timeserie';
+    if (_this.target.target != 'select_metric') _this.onChangeInternal(-1);
     return _this;
   }
 
   _createClass(GenericDatasourceQueryCtrl, [{
+    key: 'getCasc',
+    value: function getCasc(max_lev) {
+      // Returns the current casc_dataX as an array.
+      if (max_lev < 0 || max_lev > this.casc_level) max_lev = this.casc_level;
+      var casc = [];
+      for (var i = 0; i < max_lev; i++) {
+        casc.push(this['casc_data' + i]);
+      }return casc;
+    }
+  }, {
+    key: 'setCasc',
+    value: function setCasc(casc) {
+      // Loads the values from casc into this.casc_data0+ and updates casc_level.
+      this.casc_level = casc.length;
+      for (var i = 0; i < casc.length; i++) {
+        this['casc_data' + i] = casc[i];
+      }
+    }
+  }, {
     key: 'getOptions',
     value: function getOptions(query) {
-      return this.datasource.metricFindQuery(query || '');
+      this.opts_promise = this.datasource.metricFindQuery(query || '');
+      return this.opts_promise;
+    }
+  }, {
+    key: 'getCascOptions',
+    value: function getCascOptions(level) {
+      var _this2 = this;
+
+      return this.opts_promise.then(function (opts) {
+        var prefix = '';
+        if (level > 0) {
+          var casc = _this2.getCasc(level);
+          prefix = casc.join('.');
+        }
+        opts = _.map(opts, function (v) {
+          if (!v.value.startsWith(prefix)) return null;
+          if (v.value == prefix) return '<end>';
+          return v.value.split('.')[level];
+        });
+        opts = _.filter(opts, function (v, i, o) {
+          return v && o.indexOf(v) === i;
+        });
+        return _.map(opts, function (v) {
+          return { text: v, value: v };
+        });
+      });
     }
   }, {
     key: 'toggleEditorMode',
@@ -43,8 +97,39 @@ var GenericDatasourceQueryCtrl = exports.GenericDatasourceQueryCtrl = function (
     }
   }, {
     key: 'onChangeInternal',
-    value: function onChangeInternal() {
-      this.panelCtrl.refresh(); // Asks the panel to refresh data.
+    value: function onChangeInternal(level) {
+      var _this3 = this;
+
+      // Keep this.target.target and this.casc_data0- in sync.
+      //
+      // Call with level = -1 if target is changed.
+      // Call with level = N if boxN is changed.
+      if (level < 0) {
+        // Decompose target into the components.
+        var comps = this.target.target.split('.');
+        comps.push('<end>');
+        this.setCasc(comps);
+        this.panelCtrl.refresh(); // Asks the panel to refresh data.
+      } else {
+        this.getCascOptions(level).then(function (opts) {
+          var casc = _this3.getCasc(level + 1);
+          _this3.panelCtrl.refresh(); // Asks the panel to refresh data.
+          opts = _.map(opts, function (opt) {
+            return opt.value;
+          });
+          // Check if we match one of the options?
+          if (!opts.includes(casc[level])) {
+            // Set to the first option in the list.
+            casc[level] = opts[0];
+            _this3.setCasc(casc);
+          }
+          if (casc[level] != '<end>') return _this3.onChangeInternal(level + 1);
+
+          _this3.target.target = casc.slice(0, level).join('.');
+          _this3.setCasc(casc);
+          _this3.panelCtrl.refresh(); // Asks the panel to refresh data.
+        });
+      }
     }
   }]);
 
